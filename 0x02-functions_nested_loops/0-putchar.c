ea006f61a8710e92d7602a37b15bc58b9ef0495a
Abdulai <gbassay001@gmail.com>
2022-12-13 09:49:53 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+
+
+/**
+ *
+ * starting point 
+ *
+ *
+ *
+ * ending point Return 0 (Success)
+ *
+ */
+
+
+
+int main(void)
+
+
+
+{
+
+
+
+		_putchar('_');
+
+		_putchar('p');
+
+		_putchar('u');
+
+		_putchar('t');
+
+		_putchar('c');
+
+		_putchar('h');
+
+		_putchar('a');
+
+		_putchar('r');
+
+		_putchar('\n');
+
+
+
+		return (0);
+
+
+
+}
