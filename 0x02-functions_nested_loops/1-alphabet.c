dcaa95fe9b96d1a01c405fb9d5e42921cd0fa11e
Abdulai <gbassay001@gmail.com>
2022-12-13 10:09:02 -0800

    taks 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * starting point for alphabet
+ *
+ * ending point: return 0
+ *
+ */
+
+void print_alphabet(void)
+
+
+
+{
+
+		char c = 'a';
+
+
+
+		while (c <= 'z')
+
+		{
+
+			_putchar(c);
+
+			c++;
+
+		}
+
+
+
+		_putchar('\n');
+
+
+
+}
