af08e40f40464e3aa1b0b346ea270e8d159cceee
Abdulai <gbassay001@gmail.com>
2022-12-13 17:35:30 -0800

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * starting point
+ * jack_bauer - prints every minute of the day
+ * ending point: return 0
+ */
+
+void jack_bauer(void)
+
+
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 23; a++)
+	{
+		for (b = 0; b <= 59; b++)
+		{
+			_putchar (a / 10 + '0');
+			_putchar (a % 10 + '0');
+			_putchar (':');
+			_putchar (b / 10 + '0');
+			_putchar (b % 10 + '0');
+			_putchar ('\n');
+		}
+	}
+}
