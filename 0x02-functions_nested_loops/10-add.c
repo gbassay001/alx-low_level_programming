1bf7f20e691ae82930a723d95579219f94e7c038
Abdulai <gbassay001@gmail.com>
2022-12-13 17:52:25 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * starting point 
+ * add - Entry point for the addition
+ * @a: one value to be added together
+ * @b: second value to be added
+ * ending point: return 0 success
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
